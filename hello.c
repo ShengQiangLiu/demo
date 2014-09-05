f156f34d407b5d9d66d6f4d247cf9a01aa69251a
liushengqiang <liusq1993@foxmail.com>
2014-09-05 22:20:42 +0800

    hello

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@ int main()
         int x = 10;
         int y = x++ + x++;
         printf("%dxxxx\n", y);
-        printf("YYYYYYY%d\n    ", y);
+        printf("   YYYYYYY%d\n    ", y);
         return 0;
 }
